aa934b97be4919ddff920d5908b5d872813d9104
Mishel Ivanov <m.ivanov@blubito.com>
2021-04-09 11:19:19 +0300

    chnage backend port

--- backend/config/config.js
+++ backend/config/config.js
@@ -2,7 +2,7 @@ const env = process.env.NODE_ENV || 'development';
 
 const config = {
     development: {
-        PORT: process.env.PORT || 8080,
+        PORT: process.env.PORT || 8082,
         DB_CONNECTION: 'mongodb://localhost:27017/wdt',
         AUTH_HEADER_NAME: 'Authorization',
         SECRET: 'supersecretcookiesecret'

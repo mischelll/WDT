7969fd6c5e2b3105619655324b4a2f298fdf0bfd
Mishel Ivanov <m.ivanov@blubito.com>
2021-04-09 11:18:04 +0300

    make ProfileComponent a functional one

--- frontend/src/components/Profile/ProfileComponent.js
+++ frontend/src/components/Profile/ProfileComponent.js
@@ -1,35 +1,31 @@
-import { Component } from 'react';
+import { useContext, useState } from 'react';
+import {UserContext} from '../../contexts/UserContext';
 import style from './ProfileComponent.module.css'
 
-export default class ProfileComponent extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
+export default function ProfileComponent() {
+    const { currentUser } = useContext(UserContext);
+    const [email, setEmail] = useState();
 
-        }
+    function handleEmailChange(e){
+        setEmail(e.target.value)
     }
 
-    componentDidMount() {
-        console.log('hello');
-    }
+    return (
+        <div className={style.card}>
+            <form>
+                <label className={style.label}>Username: </label>
+                <input className={style.inputField} placeholder="John Doe" />
+                <label className={style.label}>Email: </label>
+                <input className={style.inputField} placeholder="johdoe@mail.com"  onChange={handleEmailChange} />
+                <label className={style.label}>Sick Days available: </label>
+                <h4>{currentUser.annualSickDaysAllowed}</h4>
+                <label className={style.label}>Vacation Days available: </label>
+                <h4>{currentUser.annualVacationDaysAllowed}</h4>
 
-    render() {
-        return (
-            <div className={style.card}>
-                <form>
-                    <label className={style.label}>Username: </label>
-                    <input className={style.inputField} placeholder="John Doe" />
-                    <label className={style.label}>Email: </label>
-                    <input className={style.inputField} placeholder="johdoe@mail.com" />
-                    <label className={style.label}>Vacation Days available: </label>
-                    <h4>14</h4>
-                    <label className={style.label}>Sick Days available: </label>
-                    <h4>4</h4>
-            
-                    <button>Save</button>
-                </form>
-            </div>
-        )
-    }
+                <button>Save</button>
+            </form>
+        </div>
+    )
+
 
 }

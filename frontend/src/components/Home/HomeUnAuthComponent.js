8a1778a400251797fd4953e74588641b6f3b150a
Mishel Ivanov <m.ivanov@blubito.com>
2021-04-10 17:33:19 +0300

    Add home unauthenticated component

---
+++ frontend/src/components/Home/HomeUnAuthComponent.js
@@ -0,0 +1,18 @@
+
+export function HomeUnAuthComponent() {
+
+
+    return (
+        <div>
+            <div>
+                <h1>WELCOME TO WDT</h1>
+            </div>
+            <div>
+                <img src="wdtlogo.png" width="550" height="550" alt="logo" />
+
+            </div>
+
+        </div>
+
+    )
+}

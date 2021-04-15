4b145e7bf0ac8d45dd6baf480eda442495693bd4
Mishel Ivanov <m.ivanov@blubito.com>
2021-04-16 00:15:44 +0300

    extend getAllSickDays

--- backend/controllers/sickDayController.js
+++ backend/controllers/sickDayController.js
@@ -1,21 +1,30 @@
 const router = require('express').Router();
-
 const sickDayService = require('../services/sickDayService');
 const isAuthenticated = require('../middleware/isAuthenticated');
+const userService = require('../services/userService');
 
-router.get('/', isAuthenticated, (req, res) => {
-    sickDayService.getAllSickDays()
-        .then(sickDays => {
+router.get('/', isAuthenticated, async (req, res) => {
+    let sickDays = await sickDayService.getAllSickDays();
+
+    let newArr = sickDays.map(async (day) => {
+        let user = await userService.getUserUsernameById(day.user);
+        day.username = user.username;
+        return Object.assign(day, { ...day, username: user.username });
+    })
+
+    Promise.all(newArr)
+        .then(mappedSickDays => {
             res.status(200);
-            res.send(sickDays)
+            res.send(mappedSickDays)
         })
         .catch(err => {
-            res.status(409);
+            res.status(404);
             res.send({
                 error: err.message,
-                code: 409,
+                code: 404,
                 time: new Date().toISOString()
             });
+
         });
 
 });

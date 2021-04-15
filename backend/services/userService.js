69f1e428587b67badbd6f1f1d531750e5b23c25d
Mishel Ivanov <m.ivanov@blubito.com>
2021-04-15 09:35:06 +0300

    add new service method: getUserUsernameById

--- backend/services/userService.js
+++ backend/services/userService.js
@@ -4,6 +4,10 @@ const getUserInfo = (userId) => {
     return User.findById({ _id: userId }).lean();
 }
 
+const getUserUsernameById = async (userId) => {
+    return await User.findById({_id: userId}).select('username').exec();
+}
 module.exports = {
     getUserInfo,
+    getUserUsernameById
 }

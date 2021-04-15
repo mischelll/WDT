9c944a6d85138d0c866bf571f9522762b21b350e
Mishel Ivanov <m.ivanov@blubito.com>
2021-04-15 09:35:42 +0300

    add roleCOntroller to routes

--- backend/routes.js
+++ backend/routes.js
@@ -7,6 +7,7 @@ const vacationDayController = require('./controllers/vacationDayController');
 const sickDayController = require('./controllers/sickDayController');
 const adminController = require('./controllers/adminController');
 const userController = require('./controllers/userController');
+const roleController = require('./controllers/roleController');
 
 router.use('/api', homeController);
 router.use('/api/auth', authController);
@@ -14,6 +15,7 @@ router.use('/api/vacationDay', vacationDayController);
 router.use('/api/sickDay', sickDayController);
 router.use('/api/admin', adminController);
 router.use('/api/user', userController);
+router.use('/api/role', roleController);
 
 router.get('*', (req, res) => {
     res.json({ message: 'error page' });
